8c95f2522d721aa29772e6d76057174fb19e4bd0
Rifazul <rifazul50@gmail.com>
2022-11-10 06:29:49 +0600

    add delete method

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ async function run(){
 
    })
 
+     
+
 
    app.post('/reviews', async(req,res)=>{
         
@@ -97,6 +99,15 @@ res.send(result)
 
 
 
+   app.delete('/reviews/:id', async(req,res)=>{
+
+        const id = req.params.id;
+        const query = { _id:ObjectId(id)}
+
+        const result = await reviewCollection.deleteOne(query)
+
+        res.send(result)
+   })
 
     }
 
